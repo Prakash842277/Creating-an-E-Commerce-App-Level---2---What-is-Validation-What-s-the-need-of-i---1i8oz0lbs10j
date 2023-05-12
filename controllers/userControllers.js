99ae9bc409ab59b42092a048e6213f912ba4722e
Prakash Chandra <prakash140chandra@gmail.com>
2023-05-12 13:10:35 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,214 @@
+// const User = require('../models/User');
+
+// /*
+
+// Implement a controller to retrieve a paginated list of users from the database.
+
+// Instructions:
+
+// The input for pagination will be passed as query parameters in the request object.
+// The default values for page and limit will be 1 and 5 respectively.
+// The User model should be imported before the function.
+// The function should use the User.find() method to retrieve the list of users from the database.
+// The function should use the limit and skip methods to implement pagination.
+// The function should retrieve the total count of users using the User.countDocuments() method.
+// The function should return a JSON response with a status code of 200 and the following format:
+// {
+// "status": "success",
+// "data": {
+// "count": <total number of users in the database>,
+// "users": <an array of user objects>
+// }
+// }
+// If no users are found, the function should return a JSON response with a status code of 404 and the following format:
+// {
+// "message": "Users Not Found",
+// "status": "Error",
+// "error": <the error object>
+// }
+// */
+// const getAllUsers = async (req, res) => {
+//     try {
+//         // Implement the function here
+//     } catch (err) {
+//         res.status(404).json({
+//             message: "Users Not Found",
+//             status: "Error",
+//             error: err,
+//         });
+//     }
+// };
+
+// /*
+
+// Implement a controller to retrieve the details of a single user with the given ID.
+
+// Instructions:
+
+// The ID of the user to be retrieved will be passed as a parameter in the request object.
+// The User model should be imported before the function.
+// The function should use the User.findById() method to retrieve the user details from the database.
+// If the user is not found, the function should return a JSON response with a status code of 404 and the following format:
+// {
+// "status": "Error",
+// "message": "User Not Found"
+// }
+// If the user is found, the function should return a JSON response with a status code of 200 and the following format:
+// {
+// "status": "success",
+// "data": {
+// "user": <user object>
+// }
+// }
+// If an error occurs during the retrieval, the function should return a JSON response with a status code of 400 and the following format:
+// {
+// "message": "User Fetching Failed",
+// "status": "Error",
+// "error": <the error object>
+// }
+// */
+// const getUserByID = async (req, res) => {
+//     try {
+//         // Implement the function here
+//     } catch (err) {
+//         res.status(400).json({
+//             message: "User Fetching Failed",
+//             status: "Error",
+//             error: err,
+//         });
+//     }
+// };
+
+// /*
+
+// Implement a controller to create a new user.
+
+// Instructions:
+
+// The input for the new user will be passed as a JSON object in the request body.
+// The User model should be imported before the function.
+// The function should check if the username, email, and password are present in the request body. If any of them are missing, the function should return a JSON response with a status code of 400 and the following format:
+// {
+// "message": "<field name> Missing",
+// "status": "Error",
+// }
+// If all the required fields are present, the function should use the User.create() method to create a new user with the provided details.
+// If the user is created successfully, the function should return a JSON response with a status code of 201 and the following format:
+// {
+// "status": "success",
+// "message": "User Created Successfully",
+// "data": {
+// "user": <user object>
+// }
+// }
+// If an error occurs during the creation, the function should return a JSON response with a status code of 400 and the following format:
+// {
+// "message": "User Creation Failed",
+// "status": "Error",
+// "error": <the error object>
+// }
+// */
+// const createUser = async (req, res) => {
+//     try {
+//         // Implement the function here
+//     } catch (err) {
+//         res.status(400).json({
+//             message: "User Creation failed",
+//             status: "Error",
+//             error: err,
+//         });
+//     }
+// };
+
+// /*
+
+// Implement a controller to update user details.
+
+// Instructions:
+
+// The function should accept the updated user details as a JSON object in the request body.
+// The User model should be imported before the function.
+// The function should check if the required fields are present in the request body. If any of them are missing, the function should return a JSON response with a status code of 400 and the following format:
+// {
+// "message": "<field name> Missing",
+// "status": "Error",
+// }
+// If all the required fields are present, the function should use the User.findByIdAndUpdate() method to update the user with the provided details.
+// If the user is updated successfully, the function should return a JSON response with a status code of 200 and the following format:
+// {
+// "status": "success",
+// "message": "User Updated Successfully",
+// "data": {
+// "updatedUser": <user object>
+// }
+// }
+// If the user to be updated is not found, the function should return a JSON response with a status code of 404 and the following format:
+// {
+// "message": "User Not Found",
+// "status": "Error",
+// }
+// If an error occurs during the updation, the function should return a JSON response with a status code of 400 and the following format:
+// {
+// "message": "User Updation Failed",
+// "status": "Error",
+// "error": <the error object>
+// }
+// */
+// const updateUser = async (req, res) => {
+//     try {
+//         // Write your function here
+//     } catch (err) {
+//         res.status(400).json({
+//             message: "User Updation Failed",
+//             status: "Error",
+//             error: err,
+//         });
+//     }
+// };
+
+// /*
+
+// Implement a controller to delete a user.
+
+// Instructions:
+
+// The function should accept the user ID to be deleted as a parameter in the request URL.
+// The User model should be imported before the function.
+// The function should use the User.findByIdAndDelete() method to delete the user with the provided ID.
+// If the user is deleted successfully, the function should return a JSON response with a status code of 201 and the following format:
+// {
+// "status": "success",
+// "message": "User Deleted Successfully",
+// "data": {
+// "user": <deleted user object>
+// }
+// }
+// If the user to be deleted is not found, the function should return a JSON response with a status code of 404 and the following format:
+// {
+// "message": "User Not Found",
+// "status": "Error",
+// }
+// If an error occurs during the deletion, the function should return a JSON response with a status code of 400 and the following format:
+// {
+// "message": "User Deletion Failed",
+// "status": "Error",
+// "error": <the error object>
+// }
+// */
+// const deleteUser = async (req, res) => {
+//     try {
+//         // Write your function here.
+//     } catch (err) {
+//         res.status(400).json({
+//             message: "User Deletion Failed",
+//             status: "Error",
+//             error: err,
+//         });
+//     }
+// };
+
+// module.exports = { getAllUsers, getUserByID, createUser, updateUser, deleteUser };
+
 const User = require('../models/User');
 
 /*
@@ -29,7 +240,33 @@ If no users are found, the function should return a JSON response with a status 
 */
 const getAllUsers = async (req, res) => {
     try {
-        // Implement the function here
+        let {limit,page} = req.query;
+
+        if(isNaN(limit)) limit = 5
+        if(isNaN(page)) page = 1
+
+        limit ??= 5
+        page ??= 1 
+
+        limit = Number(limit)
+        page = Number(page)
+        
+        let user = await User.find().limit(limit * 1).skip((page - 1) * limit)
+        if(!user){
+            return res.status(404).send({
+                "message": "Users Not Found",
+                "status": "Error",
+                "error": new Error()
+                })
+        }
+        res.status(200).send({
+            "status": "success",
+            "data": {
+                "count": 15,
+                 "users": user
+            }
+        });
+
     } catch (err) {
         res.status(404).json({
             message: "Users Not Found",
@@ -69,7 +306,23 @@ If an error occurs during the retrieval, the function should return a JSON respo
 */
 const getUserByID = async (req, res) => {
     try {
-        // Implement the function here
+       
+        let founduser = await User.findById(req.params.id);
+        
+        if (!founduser) {
+            return res.status(404).json({
+                status: "Error",
+                message: "User Not Found",
+            });
+        }
+
+        res.status(200).send({
+            "status": "success",
+            "data": {               
+                "user": founduser
+            }
+        })
+
     } catch (err) {
         res.status(400).json({
             message: "User Fetching Failed",
@@ -110,8 +363,28 @@ If an error occurs during the creation, the function should return a JSON respon
 */
 const createUser = async (req, res) => {
     try {
-        // Implement the function here
-    } catch (err) {
+        const {username,email,password,role} = req.body
+        if(!username || !email || !password || !role){
+           return res.status(400).send({
+                
+                "message": "User Creation Failed",
+                "status": "Error",
+                "error": "Fill all Fields"
+                
+            })
+        }
+        let user = new User({username,email,password})
+        await user.save()
+
+        res.status(201).send({
+            "status": "success",
+            "message": "User Created Successfully",
+            "data": {
+                "user": user
+            }
+        })
+    } 
+    catch (err) {
         res.status(400).json({
             message: "User Creation failed",
             status: "Error",
@@ -156,7 +429,23 @@ If an error occurs during the updation, the function should return a JSON respon
 */
 const updateUser = async (req, res) => {
     try {
-        // Write your function here
+        let id = req.params.id       
+        const user = await User.findByIdAndUpdate(
+        id,
+        { $set: req.body.updatedData }
+        )
+        if(!user){
+            return res.status(404).send({"message": "User Not Found",
+            "status": "Error",})
+        }
+
+        res.status(200).send({
+            "status": "success",
+            "message": "User Updated Successfully",
+            "data": {
+            "updatedUser": user }
+        })
+       
     } catch (err) {
         res.status(400).json({
             message: "User Updation Failed",
@@ -197,7 +486,19 @@ If an error occurs during the deletion, the function should return a JSON respon
 */
 const deleteUser = async (req, res) => {
     try {
-        // Write your function here.
+        const user = await User.findByIdAndDelete(req.params.id);
+        if(!user){
+            return res.status(404).send({
+                "message": "User Not Found",
+                "status": "Error",
+            })
+        }
+        res.status(201).send({
+            "status": "success",
+            "message": "User Deleted Successfully",
+            "data": {
+                "user": user }
+        })
     } catch (err) {
         res.status(400).json({
             message: "User Deletion Failed",
@@ -208,4 +509,3 @@ const deleteUser = async (req, res) => {
 };
 
 module.exports = { getAllUsers, getUserByID, createUser, updateUser, deleteUser };
-
